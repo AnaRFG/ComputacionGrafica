6214a8b50c5a3712abbc60c9f7bd4ca7a89b241f
AnaRFG <2304151@ucc.edu.ar>
2025-10-15 21:25:14 -0300

    cambio en el model

--- RayCasting_GPU/src/model.py
+++ RayCasting_GPU/src/model.py
@@ -1,38 +1,71 @@
-class Vertex:
-    def __init__(self, name, format, array):
-        self.__name = name
-        self.__format = format
-        self.__array = array
-    
-    @property
-    def name(self):
-        return self.__name
-    @property
-    def format(self):
-        return self.__format
-    @property
-    def array(self):
-        return self.__array
-    
-class VertexLayout:
-    def __init__(self):
-        self.__attributes = []
+from model import Model
+from hit import HitBox
+import numpy as np
+import glm
 
-    def add_attribute(self, name: str, format: str, array):
-        self.__attributes.append(Vertex(name, format, array))
+class Cube(Model):
+    def __init__(self, position=(0,0,0), rotation=(0,0,0), scale=(1,1,1), name="cube",animated = True, hittable = True):
+        self.name = name
+        self.animated = animated
+        self.position = glm.vec3(*position)
+        self.rotation = glm.vec3(*rotation)
+        self.scale = glm.vec3(*scale)
+        self.__colision = HitBox(get_model_matrix = lambda: self.get_model_matrix(), hittable = hittable)
 
-    def get_attributes(self):
-        return self.__attributes
+
+        vertices = np.array([
+            -1,-1,-1,  1,-1,-1,  1,1,-1, -1,1,-1,
+            -1,-1,1,   1,-1,1,   1,1,1,  -1,1,1
+        ], dtype='f4')
+
+
+        colors = np.array([
+            1,0,0, 0,1,0, 0,0,1, 1,1,0,
+            1,0,1, 0,1,1, 1,1,1, 0,0,0
+        ], dtype='f4')
+
+
+        normals = np.array([
+            -1,-1,-1,  1,-1,-1,  1,1,-1, -1,1,-1,
+            -1,-1,1,   1,-1,1,   1,1,1,  -1,1,1
+        ], dtype='f4')
+
+
+        texcoords = np.array([
+            0,0, 1,0, 1,1, 0,1,
+            0,0, 1,0, 1,1, 0,1
+        ], dtype='f4')
+
+
+        indices = np.array([
+            0,1,2, 2,3,0, 4,5,6, 6,7,4,
+            0,4,7, 7,3,0, 1,5,6, 6,2,1,
+            3,2,6, 6,7,3, 0,1,5, 5,4,0
+        ], dtype='i4')
+
+
+        super().__init__(vertices, indices, colors, normals, texcoords)
+
+
+    def check_hit(self, origin, direction):
+        return self.__colision.check_hit(origin, direction)
+   
+    def get_model_matrix(self):
+        model = glm.mat4(1)
+        model = glm.translate(model, self.position)
+        model = glm.rotate(model, glm.radians(self.rotation.x % 360), glm.vec3(1, 0, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.y % 360), glm.vec3(0, 1, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.z % 360), glm.vec3(0, 0, 1))
+        model = glm.scale(model, self.scale)
+        return model
     
-class Model:
-    def __init__(self, vertices = None, indices = None,colors = None , normals = None, texcoords = None):
-        self.indices = indices 
-        self.vertex_layout = VertexLayout()
-        if vertices is not None:
-            self.vertex_layout.add_attribute("in_pos","3f", vertices)
-        if colors is not None:
-            self.vertex_layout.add_attribute("in_color","3f", colors)
-        if normals is not None:
-            self.vertex_layout.add_attribute("in_normal", "3f", normals)
-        if texcoords is not None:
-            self.vertex_layout.add_attribute("in_uv","2f",texcoords)
+    @property
+    def aabb(self):
+        # Bounding box alineado a los ejes (Axis-Aligned Bounding Box)
+        half_scale = self.scale  # tamaño relativo al modelo base
+        min_bound = self.position - half_scale
+        max_bound = self.position + half_scale
+        return min_bound, max_bound
+
+    
+        
